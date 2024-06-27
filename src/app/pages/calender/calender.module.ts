c9be31a9b64b9518c4c10ada45110b864a6294bf
Kruti Chauhan <krutichauhan2003@gmail.com>
2024-06-27 14:31:05 +0530

    change

--- src/app/pages/calender/calender.module.ts
+++ src/app/pages/calender/calender.module.ts
@@ -7,10 +7,12 @@ import { IonicModule } from '@ionic/angular';
 import { CalenderPageRoutingModule } from './calender-routing.module';
 
 import { CalenderPage } from './calender.page';
+import { NgCalendarModule } from 'ionic2-calendar';
 
 @NgModule({
   imports: [
     CommonModule,
+    NgCalendarModule,
     FormsModule,
     IonicModule,
     CalenderPageRoutingModule
--- src/app/pages/dashboard/dashboard.page.ts
+++ src/app/pages/dashboard/dashboard.page.ts
@@ -106,96 +106,6 @@ export class DashboardPage implements OnInit {
       message: "Hello, How can I help you",
       date: "11/11/2022",
     },
-    {
-      name: "Angela Young",
-      message: "Hello, How can I help you",
-      date: "11/11/2022",
-    },
-    {
-      name: "Angela Young",
-      message: "Hello, How can I help you",
-      date: "11/11/2022",
-    },
-    {
-      name: "Angela Young",
-      message: "Hello, How can I help you",
-      date: "11/11/2022",
-    },
-    {
-      name: "Angela Young",
-      message: "Hello, How can I help you",
-      date: "11/11/2022",
-    },
-    {
-      name: "Angela Young",
-      message: "Hello, How can I help you",
-      date: "11/11/2022",
-    },
-    {
-      name: "Angela Young",
-      message: "Hello, How can I help you",
-      date: "11/11/2022",
-    },
-    {
-      name: "Angela Young",
-      message: "Hello, How can I help you",
-      date: "11/11/2022",
-    },
-    {
-      name: "Angela Young",
-      message: "Hello, How can I help you",
-      date: "11/11/2022",
-    },
-    {
-      name: "Angela Young",
-      message: "Hello, How can I help you",
-      date: "11/11/2022",
-    },
-    {
-      name: "Angela Young",
-      message: "Hello, How can I help you",
-      date: "11/11/2022",
-    },
-    {
-      name: "Angela Young",
-      message: "Hello, How can I help you",
-      date: "11/11/2022",
-    },
-    {
-      name: "Angela Young",
-      message: "Hello, How can I help you",
-      date: "11/11/2022",
-    },
-    {
-      name: "Angela Young",
-      message: "Hello, How can I help you",
-      date: "11/11/2022",
-    },
-    {
-      name: "Angela Young",
-      message: "Hello, How can I help you",
-      date: "11/11/2022",
-    },
-    {
-      name: "Angela Young",
-      message: "Hello, How can I help you",
-      date: "11/11/2022",
-    },
-    {
-      name: "Angela Young",
-      message: "Hello, How can I help you",
-      date: "11/11/2022",
-    },
-    {
-      name: "Angela Young",
-      message: "Hello, How can I help you",
-      date: "11/11/2022",
-    },
-    {
-      name: "Angela Young",
-      message: "Hello, How can I help you",
-      date: "11/11/2022",
-    },
     // Add more items to this list
   ];
 
@@ -210,42 +120,6 @@ export class DashboardPage implements OnInit {
     public util: UtilService
   ) {}
 
-
-  @ViewChild('doughnutCanvas') private doughnutCanvas: ElementRef;
-  doughnutChart: any;
-
-  ngAfterViewInit() {
-  
-    this.doughnutChartMethod();
-  }
-
-  
-  doughnutChartMethod() {
-    this.doughnutChart = new Chart(this.doughnutCanvas.nativeElement, {
-      type: 'doughnut',
-      data: {
-        labels: ['BJP', 'Congress', 'AAP', 'CPM', 'SP'],
-        datasets: [{
-          label: '# of Votes',
-          data: [50, 29, 15, 10, 7],
-          backgroundColor: [
-            'rgba(255, 159, 64, 0.2)',
-            'rgba(255, 99, 132, 0.2)',
-            'rgba(54, 162, 235, 0.2)',
-            'rgba(255, 206, 86, 0.2)',
-            'rgba(75, 192, 192, 0.2)'
-          ],
-          hoverBackgroundColor: [
-            '#FFCE56',
-            '#FF6384',
-            '#36A2EB',
-            '#FFCE56',
-            '#FF6384'
-          ]
-        }]
-      }
-    });
-  }
 
 
 
