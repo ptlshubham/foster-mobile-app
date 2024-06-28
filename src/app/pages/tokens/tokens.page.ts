66b65a3fa196661ae082b383305b0fd9c92c006a
Krish Patel <patelkrish2804@gmail.com>
2024-06-28 13:02:56 +0530

    kp changes

--- src/app/pages/tokens/tokens.page.ts
+++ src/app/pages/tokens/tokens.page.ts
@@ -10,7 +10,7 @@ import { Router } from "@angular/router";
   styleUrls: ["./tokens.page.scss"],
 })
 export class TokensPage implements OnInit {
-  tabs = "daily";
+  tabs = "token";
   tokenTabs: string = "all";
 
   constructor(
