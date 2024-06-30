7077832aa9d6bbbddd559b917ebde71c6e8c39e3
Kruti Chauhan <krutichauhan2003@gmail.com>
2024-06-30 18:51:21 +0530

    monthly

--- src/app/pages/new-token/new-token.page.ts
+++ src/app/pages/new-token/new-token.page.ts
@@ -1,58 +1,135 @@
-import { Component, Input, OnInit } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { ModalController } from '@ionic/angular';
-
-
-interface Client {
-  id: number;
-  name: string;
-}
-
-interface Manager {
-  id: number;
-  name: string;
-}
-
-interface Designer {
-  id: number;
-  name: string;
-}
 
 @Component({
   selector: 'app-new-token',
   templateUrl: './new-token.page.html',
   styleUrls: ['./new-token.page.scss'],
 })
-
 export class NewTokenPage implements OnInit {
-  @Input() staffModel: { role: string }; // Input for staff role
-  @Input() employeeList: Client[]; // Input for employee list
-  @Input() clientlist: Client[]; // Input for client list
-  @Input() managerlist: Manager[]; // Input for manager list
-  @Input() designerlist: Designer[]; // Input for designer list
-  @Input() selectedDeliveryDate: string; // Input for selected delivery date
+  clients: { value: string, clientName: string }[] = [
+    {
+      clientName: 'Shiv Dugdhalay',
+      value: '1',
+    },
+    {
+      clientName: 'GuruKrupa Jewellers',
+      value: '2',
+    },
+    {
+      clientName: 'Infinity Cinema',
+      value: '3',
+    },
+    {
+      clientName: 'Sumeru Academy',
+      value: '4',
+    },
+    {
+      clientName: 'New Anand Sport',
+      value: '5',
+    },
+    {
+      clientName: 'R & S Cinema',
+      value: '6',
+    },
+    {
+      clientName: 'Grill Kitchen Manjalpur',
+      value: '7',
+    },
+    // Add more clients as needed
+  ];
 
-  selectedClient: Client;
-  selectedManager: Manager;
-  selectedDesigner: Designer;
-  
-  constructor(private modalCtrl: ModalController) { }
+  manager: { value: string, managerName: string }[] = [
+  {
+    managerName : 'Divyangi Soni',
+    value : '1',
+  },
+  {
+    managerName : 'Harshil Amin',
+    value : '2',
+  },
+  ];
+   
+  designer : { value: string, designerName: string }[] = [
+    {
+      designerName : 'Priya Dave',
+      value : '1',
+    },
+    {
+      designerName : 'Vishal Prajapati',
+      value : '2',
+    },
+    {
+      designerName : 'Yaksh Patel',
+      value : '3',
+    },
+    {
+      designerName : 'Marvin Parmar',
+      value : '4',
+    },
+    {
+      designerName : 'Kishan Panchal',
+      value : '5',
+    },
+    {
+      designerName : 'Jenish Vaghela',
+      value : '6',
+    },
+    {
+      designerName : 'Mahir Rana',
+      value : '7',
+    },
+    ];
 
-  ngOnInit() {
+   label : { value: string, label: string }[] = [
+    {
+      label : 'CES',
+      value : '1',
+    },
+    {
+      label : 'Urgent',
+      value : '2',
+    },
+   ];
+
+  visibleOptions: { value: string, label: string }[] = [];
+
+  constructor(private modalCtrl: ModalController) {
+    this.loadVisibleOptions();
   }
 
-  onClientChange(event: { component: any; value: any }) {
-    this.selectedClient = event.value;
+  loadVisibleOptions() {
+    // Load initial set of options (first 5)
+    this.visibleOptions = this.clients.slice(0, 5).map(client => ({
+      value: client.value,
+      label: client.clientName
+    }));
   }
 
-  onManagerChange(event: { component: any; value: any }) {
-    this.selectedManager = event.value;
+  loadMoreOptions(event: any) {
+    const startIndex = this.visibleOptions.length;
+    const newOptions = this.clients.slice(startIndex, startIndex + 5).map(client => ({
+      value: client.value,
+      label: client.clientName
+    }));
+    this.visibleOptions = this.visibleOptions.concat(newOptions);
+
+    // Complete the infinite scroll action
+    event.target.complete();
+
+    // Disable infinite scroll if all options are loaded
+    if (this.visibleOptions.length >= this.clients.length) {
+      event.target.disabled = true;
+    }
   }
 
-  onDesignerChange(event: { component: any; value: any }) {
-    this.selectedDesigner = event.value;
+  ngOnInit() {}
+
+  onSelectChange(event: any) {
+    console.log('Selected value:', event.detail.value);
   }
 
-  dismiss(){
+  dismiss() {
     this.modalCtrl.dismiss();
   }
 }
